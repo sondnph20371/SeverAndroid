5028056e2a61de7865dacbdbea9c7b4da2300add
Đào Ngọc Sơn <sondnph203712>
2023-03-20 07:47:13 +0700

    maytinhhbs

--- Lab4/lab4.js
+++ Lab4/lab4.js
@@ -7,6 +7,24 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.render('home', {layout: 'main'});
 });
+
+app.get('/tinhtoan', (req, res) => {
+  res.render('default', { 
+    layout: 'main',
+    soA: 5,
+    soB:7,
+    kq: 22,
+    operator: 'cong',
+  });
+});
+
+app.get('/maytinh', (req, res) => {
+  res.render('default', { 
+    layout: 'maytinh'
+    
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
